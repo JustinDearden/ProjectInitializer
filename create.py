401e4aade9c8ed70572f9d1924ee9a6e8b150149
Justin Dearden <deardenj@uwindsor.ca>
2019-06-12 15:31:54 -0400

    updates
    minor syntax changes

--- create.py
+++ create.py
@@ -2,14 +2,14 @@ import sys
 import os
 from github import Github
 
-path = "/Users/" #Set the path to where you'll store your projects
+path = "/Users/.../" #Set the path to where you'll store your projects
 
 username = "" #Insert your github username here
 password = "" #Insert your github password here
 
 def create():
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(sys.argv[1]))
+    os.makedirs(path + folderName)
     user = Github(username, password).get_user()
     repo = user.create_repo(sys.argv[1])
     print("Succesfully created repository {}".format(sys.argv[1]))
